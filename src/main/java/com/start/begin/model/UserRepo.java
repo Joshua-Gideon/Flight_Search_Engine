8fbbe63c8a6f0347aa69c9cda8a802babd582034
Joshua-Gideon <joshuaderyl@gmail.com>
2020-09-04 20:02:41 +0530

    here comes

---
+++ src/main/java/com/start/begin/model/UserRepo.java
@@ -0,0 +1,9 @@
+package com.start.begin.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer>{
+
+	User findByName(String userName);
+
+}
